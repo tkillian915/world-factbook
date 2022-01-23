2e75b9c105b87a72f51e84e314bf9f5aee399a93
Tom Killian <tck0006@mix.wvu.edu>
2022-01-23 16:19:33 -0500

    Added icons support

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'fm-topnavbar',
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
+})
+
+export class TopNavBarComponent
+{
+    
+}
