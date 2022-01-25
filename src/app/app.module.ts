e98c9b71722f9791dd9291452a2e26686946ef10
Tom Killian <tck0006@mix.wvu.edu>
2022-01-24 20:34:37 -0500

    Added footer outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from 'src/app/header/header.component';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footercomponent';
 import { SecondHeaderComponent } from './second header/second.header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SecondHeaderComponent
+    SecondHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
