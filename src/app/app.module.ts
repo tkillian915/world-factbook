288a8f6672bfbbe7e1ac179f958e4ceb1fcd8df9
Tom Killian <tck0006@mix.wvu.edu>
2022-01-25 19:14:15 -0500

    Add card to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from 'src/app/header/header.component';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footercomponent';
+import { InfoCard1Component } from './info-cards/info.card1.component';
 import { SecondHeaderComponent } from './second header/second.header.component';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { SecondHeaderComponent } from './second header/second.header.component';
     AppComponent,
     HeaderComponent,
     SecondHeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InfoCard1Component
   ],
   imports: [
     BrowserModule
