d6648f3ad2c0dcebf0ec628f4ca1eef85fb7108f
Tom Killian <tck0006@mix.wvu.edu>
2022-01-25 19:21:37 -0500

    Changed filenames

---
+++ src/app/info-cards/infoCard1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'wf-infoCard1',
+    templateUrl: 'infoCard1.component.html',
+    styleUrls: ['infoCard1.component.css']
+})
+
+export class InfoCard1Component
+{
+    
+}
