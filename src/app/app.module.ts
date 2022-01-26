469b185851b8ccbc9e52d91e4999aa8c0e082bb4
Tom Killian <tck0006@mix.wvu.edu>
2022-01-25 20:05:10 -0500

    Added second info card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from 'src/app/header/header.component';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footercomponent';
 import { InfoCard1Component } from './info-cards/infoCard1.component';
+import { InfoCard2Component } from './info-cards/infoCard2.component';
 import { SecondHeaderComponent } from './second header/second.header.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SecondHeaderComponent } from './second header/second.header.component';
     HeaderComponent,
     SecondHeaderComponent,
     FooterComponent,
-    InfoCard1Component
+    InfoCard1Component,
+    InfoCard2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/info-cards/infoCard2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'wf-infoCard2',
+    templateUrl: 'infoCard2.component.html',
+    styleUrls: ['infoCard2.component.css']
+})
+
+export class InfoCard2Component
+{
+    
+}
