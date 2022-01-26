f413a80c01e9511db84819b2469741cfcde53e35
Tom Killian <tck0006@mix.wvu.edu>
2022-01-25 19:46:36 -0500

    Implemented card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from 'src/app/header/header.component';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footercomponent';
-import { InfoCard1Component } from './info-cards/info.card1.component';
+import { InfoCard1Component } from './info-cards/infoCard1.component';
 import { SecondHeaderComponent } from './second header/second.header.component';
 
 @NgModule({
