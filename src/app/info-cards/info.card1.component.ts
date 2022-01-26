a79c74ef9f9c4eb821da11b65cbaa198fed8d4b9
Tom Killian <tck0006@mix.wvu.edu>
2022-01-25 19:13:25 -0500

    Added info card 1

---
+++ src/app/info-cards/info.card1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'wf-info.card',
+    templateUrl: 'info.card1.component.html',
+    styleUrls: ['info.card1.component.css']
+})
+
+export class InfoCard1Component
+{
+    
+}
