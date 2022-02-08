d7fa8e0483b8aa274252dc0c435c44f2c8b168ed
Tom Killian <tck0006@mix.wvu.edu>
2022-02-08 15:51:31 -0500

    Added third mock card for lab 3

--- src/app/geo-cards/geoCard_list.ts
+++ src/app/geo-cards/geoCard_list.ts
@@ -2,9 +2,10 @@ export const geoCard_list =
 [
     /*/
         My website only has two components of this
-        type and it would only be to satisfy the
-        goals of the lab if I made a mock third item
-        in this list for a component that doesn't exist
+        type and it was only to satisfy the
+        goals of the lab that I made a mock third item
+        in this list for a component that doesn't
+        actually exist on the orignal site
     /*/
     
     {
@@ -15,5 +16,10 @@ export const geoCard_list =
     {
         img: "assets/wfb-feature-oceans.jpg",
         title: "Oceans"
+    },
+
+    {
+        img: "assets/Factbook-Glyph.PNG",
+        title: "Mock Card"
     }
 ];
