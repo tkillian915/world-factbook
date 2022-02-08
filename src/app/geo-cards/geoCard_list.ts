a74dddae696d4a3ea0c6309a50c1a86bc358d8f5
Tom Killian <tck0006@mix.wvu.edu>
2022-02-07 20:41:36 -0500

    More data binding

---
+++ src/app/geo-cards/geoCard_list.ts
@@ -0,0 +1,12 @@
+export const geoCard_list =
+[
+    {
+        img: "assets/wfb-feature-regions.jpg",
+        title: "The World & Its Regions"
+    },
+
+    {
+        img: "assets/wfb-feature-oceans.jpg",
+        title: "Oceans"
+    }
+];
--- src/app/geo-cards/locations_list.ts
+++ src/app/geo-cards/locations_list.ts
@@ -1,4 +1,14 @@
 export const locations_list = 
 [
-    
-]
+    "World",
+    "Africa",
+    "Australia and Oceana",
+    "Central America",
+    "Central Asia",
+    "East and Southeast Asia",
+    "Europe",
+    "Middle East",
+    "North America",
+    "South American",
+    "South Asia"
+];
