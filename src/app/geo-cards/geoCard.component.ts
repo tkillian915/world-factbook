9597067d9da87132185701263bf2c7958b54c540
Tom Killian <tck0006@mix.wvu.edu>
2022-02-07 20:25:37 -0500

    Data binding basics

--- src/app/geo-cards/geoCard.component.ts
+++ src/app/geo-cards/geoCard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -9,7 +9,7 @@ import { Component } from "@angular/core";
 
 export class GeoCardComponent
 {
-    img: string;
+    @Input() img: string;
     title: string;
     location: string;
 
