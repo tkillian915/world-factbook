d43c70f450c5a8183dbfb17b838ba3051baae5ad
Tom Killian <tck0006@mix.wvu.edu>
2022-02-15 17:45:30 -0500

    Routing for Lab 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MapsComponent } from './maps-page/maps.component';
 import { HomeLayoutComponent } from './home-layout.component';
+import { AboutComponent } from './about-page/about.component';
+import { ReferencesComponent } from './references/references.component';
+import { WorldPageComponent } from './world-page/world-page.component';
+import { AfricaComponent } from './africa-page/africa.countries.component';
 
 const routes:Routes = 
 [
   {path: '', component: HomeLayoutComponent},
   {path: 'maps', component: MapsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'references', component: ReferencesComponent},
+  {path: 'world', component: WorldPageComponent},
+  {path: 'africa', component: AfricaComponent},
   {path: 'home', component: HomeLayoutComponent},
 ];
 
