ab8d312c1490a2b8efa5bdd64b8b11d6b7c6d12e
Tom Killian <tck0006@mix.wvu.edu>
2022-02-15 17:14:08 -0500

    Most navbar links kinda work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MapsComponent } from './maps-page/maps.component';
+import { HomeLayoutComponent } from './home-layout.component';
 
 const routes:Routes = 
 [
-  {path: 'maps', component: MapsComponent}
+  {path: '', component: HomeLayoutComponent},
+  {path: 'maps', component: MapsComponent},
+  {path: 'home', component: HomeLayoutComponent},
 ];
 
 @NgModule({
