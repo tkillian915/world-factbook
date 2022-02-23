d5bd87375fb54b48c1772bfa013a37eda63d36ca
Tom Killian <tck0006@mix.wvu.edu>
2022-02-22 20:03:28 -0500

    User info stuff

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UserInfo } from './user-info.model';
 
 @Component({
   selector: 'wf-user-info',
@@ -8,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoComponent implements OnInit {
 
+  myInfo: UserInfo | undefined;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    console.log("Sending request to server");
+    this.showUserInfo();
   }
 
+  getUserInfo()
+  {
+    return this.http.get<UserInfo>('https://world-factbook-17ada-default-rtdb.firebaseio.com/myInfo.json');
+  }
+
+  showUserInfo()
+  {
+    this.getUserInfo().subscribe((data: UserInfo) => {
+      console.log(data);
+      this.myInfo = data;
+    })
+  }
 }
---
+++ src/app/user-info/user-info.model.ts
@@ -0,0 +1,7 @@
+export interface UserInfo
+{
+    FirstName: string;
+    LastName: string;
+    GitHub: string;
+    mix_id: string;
+}
