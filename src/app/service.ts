9764825935e80eaa991ce6b843b5adc253b9a1c9
Tom Killian <tck0006@mix.wvu.edu>
2022-03-01 19:37:10 -0500

    Added service to backend

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,12 +1,23 @@
 import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { infoCardModel } from "./info-cards/infoCard_list.model";
 
-export class FactbookService
+@Injectable({
+    providedIn: 'root'
+})
+
+export class FactbookService 
 {
     private baseUrl: string = "https://world-factbook-17ada-default-rtdb.firebaseio.com/";
     private infoCardEndpoint: string = "info-cards.json";
 
-    constructor(private http:HttpClient)
+    constructor(private http: HttpClient) 
     {
-        
+
+    }
+
+    public getInfoCards()
+    {
+        return this.http.get<infoCardModel[]>(this.baseUrl + this.infoCardEndpoint);
     }
 }
