1441623ee9564a8bde25977a616762957e624790
Tom Killian <tck0006@mix.wvu.edu>
2022-04-05 15:30:57 -0400

    Added apiKey auth

---
+++ src/app/auth/authentication/authentication.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+
+@Injectable(
+    {
+        providedIn: 'root'
+    }
+)
+
+export class AuthneticationService
+{
+    baseUrl: string = "https://identitytoolkit.googleapis.com/v1/accounts";
+    signUpEndpoint : string = "signUp";
+
+    public constructor(private http:HttpClient)
+    {
+
+    }
+
+    public signUp(email:string, password:string)
+    {
+        const requestBody = 
+        {
+            "email":email,
+            "password":password,
+            "returnSecureToken":true
+        };
+
+        return this.http.post(this.baseUrl + ":" + this.signUpEndpoint + "?"
+        + "key=" + environment.firebase.apiKey, requestBody);
+    }
+}
